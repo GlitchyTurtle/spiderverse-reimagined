81d04ecd493b4c64d278acee83df8c801d603e6a
GlitchyTurtle <93545476+GlitchyTurtle@users.noreply.github.com>
2024-08-23 22:07:31 -0600

    Fixing a weird mistake I made!
    I have no idea how these files got here, probably some copy paste shenanigans.

--- BP/SPIDERVERSE/scripts/events/playerLeave.js
+++
@@ -1,8 +0,0 @@
-import { PLAYER_DATA_MAP } from "../index.js";
-
-export const playerLeave = (v) => {
-    console.warn(`Player ${v.player.name} left the server.`);
-    if (PLAYER_DATA_MAP[v.player.id]) delete PLAYER_DATA_MAP[v.player.id];
-
-    console.warn(JSON.stringify(PLAYER_DATA_MAP));
-}
